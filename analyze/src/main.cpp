eeb68639f5fd9540650abf6d4bac8879f37b2bbe
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-02-17 09:48:55 +0100

    bootstrapped c++ project

---
+++ analyze/src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  automated-privacy-risk-mhealth
+//
+//  Created by tomaszbrue on 17/02/16.
+//  Copyright © 2016 Universität zu Köln. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+
+	std::cout << "Hi there" << std::endl;
+	return 0;
+}
