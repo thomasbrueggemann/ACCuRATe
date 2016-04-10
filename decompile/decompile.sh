e14c5a79a4e358c0384258b7bcea828d5086ff68
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-04-11 00:06:18 +0200

    decompile skript fertig

--- decompile/decompile.sh
+++ decompile/decompile.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 clear
 
+INPUT=$1
+OUTPUT=$2
+
 # loop all apk files in current directory
-for FILE in ./*.apk; do
+for FILE in $INPUT/*.apk; do
 
 	FOLDER=${FILE/.apk/""}
 	FOLDER=$(echo $FOLDER| cut -c 3-100000)
 	echo $FOLDER
 
 	# create output folder for decompiled classes
-	rm -R "output/$FOLDER"
-	mkdir -p "output/$FOLDER"
+	rm -R "$OUTPUT/$FOLDER"
+	mkdir -p "$OUTPUT/$FOLDER"
 
 	# extract .jar from .apk
-	dex2jar/d2j-dex2jar.sh -o "output/$FOLDER.jar" "$FOLDER.apk"
+	dex2jar/d2j-dex2jar.sh -o "$OUTPUT/$FOLDER.jar" "$FOLDER.apk"
 
 	# extract resources from apk files
-	java -jar apktool/apktool_2.0.3.jar decode -f "$FOLDER.apk" -o "output/$FOLDER/"
-	rm -Rf "output/$FOLDER/smali"
-	rm "output/$FOLDER/apktool.yml"
+	java -jar apktool/apktool_2.0.3.jar decode -f "$FOLDER.apk" -o "$OUTPUT/$FOLDER/"
+	rm -Rf "$OUTPUT/$FOLDER/smali"
+	rm "$OUTPUT/$FOLDER/apktool.yml"
 
 	# decompile jar archive
-	java -jar fernflower/fernflower.jar -ren=1 "output/$FOLDER.jar" "output/$FOLDER/"
-	unzip "output/$FOLDER/$FOLDER.jar" -d "output/$FOLDER/"
-	rm "output/$FOLDER.jar" 
-	rm "output/$FOLDER/$FOLDER.jar"
+	java -jar fernflower/fernflower.jar -ren=1 "$OUTPUT/$FOLDER.jar" "$OUTPUT/$FOLDER/"
+	unzip "$OUTPUT/$FOLDER/$FOLDER.jar" -d "$OUTPUT/$FOLDER/"
+	rm "$OUTPUT/$FOLDER.jar" 
+	rm "$OUTPUT/$FOLDER/$FOLDER.jar"
 
 	# format the java files to make them human 
 	# readable for possible inspections
-	for f in $(find "output/$FOLDER/" -name '*.java'); do 
+	for f in $(find "$OUTPUT/$FOLDER/" -name '*.java'); do 
 		echo "Format: $f"
 		astyle/build/mac/bin/astyle -n -q --style=java -s4 -xc -S -K -j $f
 	done
